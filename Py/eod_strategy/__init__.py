56c2bd4e3b3410367d66006e9e6c5e83f2bbc553
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-09-15 13:36:51 +0200

    Update __init__.py

--- Py/eod_strategy/__init__.py
+++ Py/eod_strategy/__init__.py
@@ -1,21 +1,24 @@
-# eod_strategy/__init__.py
-
 """
 EOD Continuation Strategy package
 ---------------------------------
 Convenience imports for quick access.
 
 Usage:
-    from eod_strategy import StrategyConfig, run_strategy_on_dataframe
+    from eod_strategy import (
+        StrategyConfig, run_strategy_on_dataframe,
+        simulate_positions, compare_signals, run_core_strategy
+    )
 """
 
 from .eod_continuation import StrategyConfig, run_strategy_on_dataframe
 from .simulator import simulate_positions
 from .compare_logs import compare_signals
+from .core_strategy import run_core_strategy
 
 __all__ = [
     "StrategyConfig",
     "run_strategy_on_dataframe",
     "simulate_positions",
     "compare_signals",
+    "run_core_strategy",
 ]
