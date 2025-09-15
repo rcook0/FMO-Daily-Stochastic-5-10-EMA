01d4984264b5330bebafd62f81b20c63280b756f
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-09-15 13:11:00 +0200

    Update __main__.py

--- Py/eod_strategy/__main__.py
+++ Py/eod_strategy/__main__.py
@@ -4,6 +4,8 @@ eod_strategy.__main__
 Allows package execution with:
     python -m eod_strategy <args>
 
+    Ex.: python -m eod_strategy examples/sample_data.csv --symbol XAUUSD --out signals.csv
+
 It simply forwards to eod_continuation.main()
 """
 from .eod_continuation import main
